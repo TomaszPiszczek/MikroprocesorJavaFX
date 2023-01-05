9e029b5e647ca45d684784440c00be3892c1ced7
Tomasz <Tomasz.Piszczek@microsoft.wsei.edu.pl>
2023-01-05 11:58:37 +0100

    Dodano działania

--- src/emulator/controller/Scene2Controller.java
+++ src/emulator/controller/Scene2Controller.java
@@ -1,6 +1,7 @@
 package emulator.controller;
 
 import emulator.data.SingletonData;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -25,5 +26,53 @@ public class Scene2Controller {
         System.out.println("registerSwitchScene 1" + registerData.getRegisterName()[0].getText());
 
 
+    }
+
+    @FXML
+    void ADD(MouseEvent event) {
+    }
+
+    @FXML
+    void AND(MouseEvent event) {
+    }
+
+    @FXML
+    void DEC(MouseEvent event) {
+    }
+
+    @FXML
+    void INC(MouseEvent event) {
+    }
+
+    @FXML
+    void MOV(MouseEvent event) {
+    }
+
+    @FXML
+    void NOT(MouseEvent event) {
+    }
+
+    @FXML
+    void OR(MouseEvent event) {
+    }
+
+    @FXML
+    void SUB(MouseEvent event) {
+    }
+
+    @FXML
+    void XCHG(MouseEvent event) {
+    }
+
+    @FXML
+    void XOR(MouseEvent event) {
+    }
+
+    @FXML
+    void register1(MouseEvent event) {
+    }
+
+    @FXML
+    void register2(MouseEvent event) {
     }
 }
